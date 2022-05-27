86ed8f13d6353aaadbd9e4db7522605f2fd0e672
Elizabeth Larson <elarso2@uw.edu>
2022-05-26 23:19:23 -0700

    adding more files, starting user model/routes/seeds

---
+++ seeds/seed.js
@@ -0,0 +1,19 @@
+const sequelize = require('../config/connection');
+const { User } = require('../models');
+
+const userData = require('./userData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  // for (const    ) this will be stuff for other models. see seed.js of mini project
+
+  process.exit(0);
+};
+
+seedDatabase();
