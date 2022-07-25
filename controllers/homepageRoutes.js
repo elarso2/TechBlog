e0fa34050e1f7be6b488e0b5eb9d85ae2100ac48
Elizabeth Larson <elarso2@uw.edu>
2022-07-24 20:53:12 -0700

    homepage routes for blog all and id

--- controllers/homepageRoutes.js
+++ controllers/homepageRoutes.js
@@ -1,9 +1,65 @@
 const router = require('express').Router();
-const { User, Blog } = require('../models');
+const { User, Blog, Comment } = require('../models');
 const hasAuth = require('../utils/auth');
 
 router.get('/', (req, res) => {
-  res.render('../view/layouts.main');
+  try {
+    const blogData = await Blog.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
+
+    const blogs = blogData.map((blog) => blog.get({ plain: true }));
+
+    res.render('homepage', {
+      blogs,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/blog/:id', async (req, res) => {
+  try {
+    const blogData = await Blog.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: 'username',
+        },
+      ],
+    });
+
+    const commentData = await Comment.findAll({
+      where: {
+        blog_id: req.params.id,
+      },
+      include: [
+        {
+          model: Blog,
+          model: User,
+        },
+      ],
+    });
+
+    const blogs = blogData.get({ plain: true });
+    const comments = commentData.map((comment) => comment.get({ plain: true }));
+
+    res.render('blog', {
+      ...blogs,
+      comments,
+      blog_id: req.params.id,
+      current_user: req.params.user_id,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
