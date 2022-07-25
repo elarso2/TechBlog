b0b5374e1f532f4527124b9eadec2070468053f2
Elizabeth Larson <elarso2@uw.edu>
2022-07-24 17:57:28 -0700

    login page handlebars

--- controllers/homepageRoutes.js
+++ controllers/homepageRoutes.js
@@ -1,3 +1,7 @@
 const router = require('express').Router();
 const { User, Blog } = require('../models');
 const hasAuth = require('../utils/auth');
+
+router.get('/', (req, res) => {
+  res.render('../view/layouts.main');
+});
